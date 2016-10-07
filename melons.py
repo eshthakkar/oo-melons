6ef9761d3aa588ba1f7799858e5737478b6834c5
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-06 17:17:26 -0700

    Completed part 1: created abstract class and sub classes

---
+++ melons.py
@@ -0,0 +1,47 @@
+class AbstractMelonOrder(object):
+    """Abtract melon order class"""
+
+    def __init__(self, species, qty, order_type, tax):
+        """Initialize melon order attributes"""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+    def __init__(self, species, qty): 
+        super(DomesticMelonOrder, self).__init__(species, qty, "domestic", 0.08)   
+        
+        
+    
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code): 
+        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)  
+
+        self.country_code = country_code
+        
+      
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code

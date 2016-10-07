7c48844454467355d43f0968b5645c076be5d750
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-06 17:34:44 -0700

    Updated get_total to account for christmas melons and international shipping

--- melons.py
+++ melons.py
@@ -14,7 +14,14 @@ class AbstractMelonOrder(object):
         """Calculate price."""
 
         base_price = 5
+
+        if self.species == "Christmas_melons":
+            base_price = base_price * 1.5
+        
         total = (1 + self.tax) * self.qty * base_price
+        if self.order_type == "international" and self.qty < 10:
+            total += 3
+            
         return total
 
     def mark_shipped(self):
